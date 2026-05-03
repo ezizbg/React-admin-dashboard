fc801236c6eb0f431c032e8fdc39156e406c802c
Eziz Berdiyev <ezizbg04@gmail.com>
2026-05-03 17:08:04 +0300

    update gh

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
     <AppErrorBoundary>
       <AppQueryProvider>
         <I18nProvider>
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
             <AuthProvider>
               <ToastProvider>
                 <App />
