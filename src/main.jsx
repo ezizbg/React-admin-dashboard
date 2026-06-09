983852f53cc109b642d940f29e5e586ebebcd6f3
Eziz Berdiyev <ezizberdyev@MacBook-Air-Eziz.local>
2026-06-09 18:13:21 +0300

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "./app/App";
 import { AppErrorBoundary } from "./app/providers/ErrorBoundary";
 import { AppQueryProvider } from "./app/providers/QueryProvider";
@@ -16,13 +16,13 @@ createRoot(document.getElementById("root")).render(
       <AppQueryProvider>
         <I18nProvider>
           <ThemeProvider>
-            <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <HashRouter>
               <AuthProvider>
                 <ToastProvider>
                   <App />
                 </ToastProvider>
               </AuthProvider>
-            </BrowserRouter>
+            </HashRouter>
           </ThemeProvider>
         </I18nProvider>
       </AppQueryProvider>
